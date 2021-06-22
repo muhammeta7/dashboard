5eb1a20a56c1ee1658033ccc8346fd07f491736f
muhammeta7 <muhammeta7@gmail.com>
2021-06-21 23:27:34 -0400

    displaying lists, dictionaries, and text on sidebar

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,24 @@
-import streamlit
 import streamlit as st
 
-streamlit.title("This is the title!")
+st.title("This is the title!")
 
+st.header("This is a header!")
+
+st.write("This is regular text!")
+
+"""
+# header
+## subheader
+"""
+
+some_dictionary = {
+    "key": "value",
+    "key2": "value2"
+}
+
+some_list = [1, 2, 3]
+
+st.write(some_dictionary)
+st.write(some_list)
+
+st.sidebar.write("This will be over on the sidebar")
