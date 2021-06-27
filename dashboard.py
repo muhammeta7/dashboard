1e548f59f35c8e3ad44ae56311c6d0cdede951a8
muhammeta7 <muhammeta7@gmail.com>
2021-06-27 12:46:40 -0400

    update dash to display dataframes and images

--- dashboard.py
+++ dashboard.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("This is the title!")
 
@@ -22,3 +24,10 @@ st.write(some_dictionary)
 st.write(some_list)
 
 st.sidebar.write("This will be over on the sidebar")
+
+df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
+
+st.dataframe(df)
+
+st.image("https://www.nasdaq.com/sites/acquia.prod/files/styles/720x400/public/image/4b6c84393df1ef26f81356305cf5fba0007f8c2b_f9bcaea802f3a0a5afed32f5702d48e7.png?itok=ApkuIVqH")
+
