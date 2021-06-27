e1701b0e0517d4e4feb383a3839e67e36d263de9
muhammeta7 <muhammeta7@gmail.com>
2021-06-27 15:30:22 -0400

    refactor to retrieve stocktwits api info on AAPL

--- dashboard.py
+++ dashboard.py
@@ -1,33 +1,26 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import requests
+option = st.sidebar.selectbox("Which Dashboard?", ('twitter', 'wallstreet bets', 'stocktwits', 'chart', 'pattern'))
 
-st.title("This is the title!")
+st.header(option)
 
-st.header("This is a header!")
+if option == 'twitter':
+    st.subheader("Twitter Dashboard")
 
-st.write("This is regular text!")
+if option == 'chart':
+    st.subheader("Chart Dashboard")
 
-"""
-# header
-## subheader
-"""
+if option == 'stocktwits':
+    pass
 
-some_dictionary = {
-    "key": "value",
-    "key2": "value2"
-}
+    req = requests.get('https://api.stocktwits.com/api/2/streams/symbol/AAPL.json')
 
-some_list = [1, 2, 3]
+    data = req.json()
 
-st.write(some_dictionary)
-st.write(some_list)
-
-st.sidebar.write("This will be over on the sidebar")
-
-df = pd.DataFrame(np.random.randn(50, 20), columns=('col %d' % i for i in range(20)))
-
-st.dataframe(df)
-
-st.image("https://www.nasdaq.com/sites/acquia.prod/files/styles/720x400/public/image/4b6c84393df1ef26f81356305cf5fba0007f8c2b_f9bcaea802f3a0a5afed32f5702d48e7.png?itok=ApkuIVqH")
-
+    for message in data['messages']:
+        st.image(message['avatar_url'])
+        st.write(message['user']['username'])
+        st.write(message['created_at'])
+        st.write(message['body'])
